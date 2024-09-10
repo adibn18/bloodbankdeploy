d00a480ae0afd6d0b48dbc9afc8799886a4087f0
adibn18 <127621094+adibn18@users.noreply.github.com>
2024-09-10 18:13:55 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://adibnawaz:fEIChpbSP5Z6bdU5@bloodbank.req8ok9.mongodb.net/bloodbank");
     console.log(
       `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
         .white
